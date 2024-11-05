1327371cbdc4f32670425904c1bc119b21117467
Your Name <you@example.com>
2024-11-05 13:48:20 +0300

    commit

---
+++ utilits/fileHander.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+
+
+const filePath = path.join(__dirname, '../data/tasks.json');
+
+
+exports.writeTasksToFile = (data) => {
+    try {
+        fs.writeFileSync(filePath, JSON.stringify(data, null, 2)); 
+        return data;
+    } catch (error) {
+        console.error('Error writing to file:', error);
+        throw error; 
+    }
+};
+
+
+exports.readTasksFromFile = () => {
+   
+    if (!fs.existsSync(filePath)) {
+        exports.writeTasksToFile([]);  
+    }
+    
+    try {
+        const data = fs.readFileSync(filePath, 'utf8');  
+        return JSON.parse(data);
+    } catch (error) {
+        console.error('Error reading from file:', error);
+        throw error;  
+    }
+};
